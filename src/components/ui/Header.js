d82a87988b67f0f1e45b0198da4fbd258d3db54a
Caroline Courtney <caroline.courtney@earthwavetech.com>
2022-01-31 14:30:06 -0500

    styles and active page useState hook

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/styles';
-import Tabs from '@material-ui/core/Tabs'
-import Tab from '@material-ui/core/Tab'
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
 
 import logo from '../../assets/logo.svg';
 
@@ -36,11 +37,23 @@ const useStyles = makeStyles(theme => ({
     ...theme.typography.tab,
     minWidth: 10,
     marginLeft: "25px"
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: "50px",
+    marginLeft: "50px",
+    marginRight: "25px",
+    height: "45px"
   }
 }));
 
 export default function Header(props) {
   const classes = useStyles()
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value)
+  }
 
   return (
     <React.Fragment>
@@ -48,13 +61,21 @@ export default function Header(props) {
         <AppBar>
           <Toolbar disableGutters>
             <img alt="company logo" className={classes.logo} src={logo} />
-            <Tabs className={classes.tabContainer}>
+            <Tabs 
+              value={value} 
+              onChange={handleChange} 
+              className={classes.tabContainer} 
+              indicatorColor="primary"
+            >
               <Tab className={classes.tab} label="Home" />
               <Tab className={classes.tab} label="Services" />
               <Tab className={classes.tab} label="The Revolution" />
               <Tab className={classes.tab} label="About Us" />
               <Tab className={classes.tab} label="Contact Us" />
             </Tabs>
+            <Button variant="contained" color="secondary" className={classes.button}>
+              Free Estimate
+            </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>

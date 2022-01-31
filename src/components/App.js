b989c586e11e55939c3671ba30d2e36752d1d008
Caroline Courtney <caroline.courtney@earthwavetech.com>
2022-01-31 13:18:03 -0500

    theming and overriding basics

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../App.css';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './ui/Theme';
+import Header from '../components/ui/Header'
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      Hello!
+    </ThemeProvider>
+  );
+}
+
+export default App;

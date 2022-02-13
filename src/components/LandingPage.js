7c375d31c80230cb32a36717775c7e994bb93190
Caroline Courtney <caroline.courtney@earthwavetech.com>
2022-02-12 19:49:35 -0500

    adjust hero basic typography and animation specs

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Lottie from 'react-lottie';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/grid';
+import Button from '@material-ui/core/button';
+import ButtonArrow from '../components/ui/ButtonArrow';
 import animationData from '../animations/landinganimation/data';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
-
+  animation: {
+    maxWidth: "50em",
+    minWidth: "21em",
+    marginTop: "2em",
+    marginLeft: "10%"
+  }
 }));
 
 export default function LandingPage() {
@@ -19,5 +28,35 @@ export default function LandingPage() {
     }
   };
 
-  return <Lottie options={defaultOptions} height={"100%"} width={"100%"} />
+  return (
+    <Grid container direction="column">
+      <Grid item>
+        <Grid container justifyContent="flex-end" alignItems="center" direction="row">
+          <Grid sm item>
+            <Typography align="center" variant="h2">
+              Bringing West Coast Technology<br/>to the Midwest
+            </Typography>
+            <Grid container>
+              <Grid item>
+                <Button variant="contained">Free Estimate</Button>
+              </Grid>
+              <Grid item>
+                <Button variant="outlined">
+                  Learn More
+                  <ButtonArrow width={15} height={15} fill="red" />
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid sm item className={classes.animation}>
+            <Lottie 
+              options={defaultOptions} 
+              height={"100%"} 
+              width={"100%"} 
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -4,30 +4,37 @@ const arcBlue = "#0b72b9";
 const arcOrange = "#ffba60"
 
 export default createTheme({
-    palette: {
-        common: {
-            blue: `${arcBlue}`,
-            orange: `${arcOrange}`,
-        },
-        primary: {
-            main: `${arcBlue}`
-        },
-        secondary: {
-            main: `${arcOrange}`
-        }
+  palette: {
+    common: {
+      blue: `${arcBlue}`,
+      orange: `${arcOrange}`,
     },
-    typography: {
-        tab: {
-            fontFamily: "Raleway",
-            textTransform: "none",
-            fontWeight: "700",
-            fontSize: "1rem",
-        },
-        estimate: {
-            fontFamily: "Pacifico",
-            fontSize: "1rem",
-            textTransform: "none",
-            color: "white"
-        }
+    primary: {
+      main: `${arcBlue}`
+    },
+    secondary: {
+      main: `${arcOrange}`
     }
+},
+typography: {
+    tab: {
+      fontFamily: "Raleway",
+      textTransform: "none",
+      fontWeight: "700",
+      fontSize: "1rem",
+    },
+    estimate: {
+      fontFamily: "Pacifico",
+      fontSize: "1rem",
+      textTransform: "none",
+      color: "white"
+    },
+    h2: {
+      fontFamily: "Raleway",
+      fontWeight: 700,
+      fontSize: "2.5rem",
+      color: `${arcBlue}`,
+      lineHeight: 1.5
+    }
+  }
 });
